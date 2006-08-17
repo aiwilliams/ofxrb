1d33521abc78523057b3b22b726894439eecfc53
Adam Williams <adam@thewilliams.ws>
2006-08-17 03:58:06 +0000

    Added a few more attributes, some docs

--- lib/ofx_model.rb
+++ lib/ofx_model.rb
@@ -14,29 +14,38 @@ module OFXRB
 
 
     class Transaction < OfxObject
-      has_attr :amount, 'TRNAMT', :money
+      has_attrs :amount         => {:path => 'TRNAMT',
+                                    :type => :money},
+                :date           => 'DTPOSTED',
+                :name           => 'NAME',
+                :memo           => 'MEMO',
+                :fi_identifier  => {:path => 'FITID',
+                                    :doc => 'Unique, useful for determining whether this is already represented in client'}
     end
 
 
     class CreditCardStatement < OfxObject
-      has_attrs :identifier => 'TRNUID',
+      has_attrs :identifier => {:path => 'TRNUID',
+                                :doc => 'Client-assigned globally-unique ID for this transaction, 0 if a simple statement download'},
                 :currency   => ['CCSTMTRS', 'CURDEF'],
                 :number     => ['CCSTMTRS', 'CCACCTFROM', 'ACCTID'],
-                :start_date => {:path => ['CCSTMTRS', 'BANKTRANLIST', 'DTSTART'], :type => :datetime},
-                :end_date   => {:path => ['CCSTMTRS', 'BANKTRANLIST', 'DTEND'], :type => :datetime}
+                :start_date => {:path => ['CCSTMTRS', 'BANKTRANLIST', 'DTSTART'],
+                                :type => :datetime},
+                :end_date   => {:path => ['CCSTMTRS', 'BANKTRANLIST', 'DTEND'],
+                                :type => :datetime}
               
-      has_one :status
+      has_one   :status
 
-      has_many :transactions, ['CCSTMTRS', 'BANKTRANLIST', 'STMTTRN']
+      has_many  :transactions, ['CCSTMTRS', 'BANKTRANLIST', 'STMTTRN']
     end
 
     class OfxInstance < OfxObject
-      has_header :version
+      has_header  :version
 
       has_one     :status, ['OFX', 'SIGNONMSGSRSV1', 'SONRS', 'STATUS']
       has_one     :financial_institution, ['OFX', 'SIGNONMSGSRSV1', 'SONRS', 'FI']
 
-      has_many  :credit_card_statements, ['OFX', 'CREDITCARDMSGSRSV1', 'CCSTMTTRNRS']
+      has_many    :credit_card_statements, ['OFX', 'CREDITCARDMSGSRSV1', 'CCSTMTTRNRS']
     end
   
   end
